239c145c794443021b2432ba13acb39e568c8aac
looping42 <loopingr42@gmail.com>
2018-04-13 19:47:00 +0200

    first commit

--- PoBot/Program.cs
+++ PoBot/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace PoBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string test = "";
         }
     }
 }
